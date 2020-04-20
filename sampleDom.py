fa4c31ddef402a9e465d73851494639d206f4a19
jagadesh.p <jagadesh.p@zohocorp.com>
2020-04-21 00:40:47 +0530

    commiting changes

--- sampleDom.py
+++ sampleDom.py
@@ -1,32 +1,60 @@
 from xml.dom import minidom
-import ast
-dom = minidom.parse("components_chromium_strings.grd")
+import os
+import json
 
-#Get the text values from the given node and its child nodes
+
+def stringScan():
+    with open("duplicate.xml", "r") as file:
+        fileContents = file.read()
+    for string in defaultString:
+        count = fileContents.count(string)
+        defaultString[string] += count
+    print(defaultString)
+
+
+'''
+Get the text values from the given node and its child nodes
+'''
+
 
 def getText(nodelist):
     rc = []
     for node in nodelist.childNodes:
         if node.nodeType == node.TEXT_NODE:
-            rc.append(node.nodeValue)
+            rc.append(node.nodeValue.replace('\n', '').replace('"', ''))
     return ''.join(rc)
 
-message = dom.getElementsByTagName("message")
 
-#Creating a text file to store IDS and its equivalent text values
+# Driver code
+defaultString = {
+    "Chromium": 0,
+    "Chrome": 0,
+    "Google": 0
+}
+# dom = minidom.parse("components_chromium_strings.grd")
+# message = dom.getElementsByTagName("message")
 
-textFile = open("changes.json","w")
-textFile.write("{")
-for msg in message:
-    # textFile.write((msg.attributes['name'].value + " : "+ "".join(t.nodeValue for t in msg.childNodes if t.nodeType == t.TEXT_NODE)).encode('utf-8'))
-    textFile.write(("\""+msg.attributes['name'].value+"\"" + " : " + "\"" + getText(msg) + "\",").encode('utf-8'))
-textFile.write("}")
-textFile.close()
+# # Creating a text file to store IDS and its equivalent text values as json
 
+# textFile = open("changes.json", "w")
+# textFile.write("{\n")
+# for msg in message:
+#     # textFile.write((msg.attributes['name'].value + " : "+ "".join(t.nodeValue for t in msg.childNodes if t.nodeType == t.TEXT_NODE)).encode('utf-8'))
+#     textFile.write(("\""+msg.attributes['name'].value+"\"" +
+#                     " : " + "\"" + getText(msg) + "\",\n").encode("utf-8"))
 
-#Get the file content from text file as dict
+# textFile.seek(-2, os.SEEK_END)
+# textFile.truncate()
+# textFile.write("\n}")
+# textFile.close()
 
-textFile = open("changes.json","r")
-fileContent = ast.literal_eval(textFile.read())
-print(fileContent)
-textFile.close()
+# # Get the file content from text file as dict
+
+# textFile = open("changes.json", 'r')
+# fileContent = (textFile.read())
+# # fileContent = fileContent.replace('\n', '')
+# jsonDict = json.loads(fileContent)
+# print(jsonDict["IDS_DEPRECATED_FEATURES_RELAUNCH_NOTICE"])
+# textFile.close()
+
+stringScan()
